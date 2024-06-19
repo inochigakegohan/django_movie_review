d84aaf5a1b68707e1ca8e820aaa6d48b89182bea
inochigakegohan <kishiura6202@gmail.com>
2024-06-19 09:23:08 +0900

    feat: 映画のデータベースにデータモデルを定義

--- movie_review/cms/models.py
+++ movie_review/cms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """映画"""
+    name = models.CharField('映画名', max_length=255)
+    publisher = models.CharField('配給会社', max_length=255, blank=True)
+    page = models.IntegerField('上映時間', blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Impression(models.Model):
+    """感想"""
+    book = models.ForeignKey(Movie, verbose_name='映画名', related_name='impressions', on_delete=models.CASCADE)
+    comment = models.TextField('コメント', blank=True)
+
+    def __str__(self):
+        return self.comment
