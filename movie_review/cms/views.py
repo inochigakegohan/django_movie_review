fd57dc0ed5d9c3e6841491db0648658c53eaa8e2
inochigakegohan <kishiura6202@gmail.com>
2024-06-21 08:52:40 +0900

    feat: 一覧、登録、修正、削除のファンクションを作成

--- movie_review/cms/views.py
+++ movie_review/cms/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def movie_list(request):
+    """映画の一覧"""
+    return HttpResponse('映画の一覧')
+
+
+def movie_edit(request, movie_id=None):
+    """映画の編集"""
+    return HttpResponse('映画の編集')
+
+
+def movie_del(request, movie_id):
+    """映画の削除"""
+    return HttpResponse('映画の削除')
