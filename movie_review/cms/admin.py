f3517b4ea60eb88c2e52e4cfc42ed28c350b4053
inochigakegohan <kishiura6202@gmail.com>
2024-06-20 09:18:52 +0900

    feat: 一覧に出したい項目を追加、修正リンクでクリックできる項目を追加

--- movie_review/cms/admin.py
+++ movie_review/cms/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from .models import Movie, Impression
 
-admin.site.register(Movie)
-admin.site.register(Impression)
+# admin.site.register(Movie)
+# admin.site.register(Impression)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'company', 'time',)  # 一覧に出したい項目
+    list_display_links = ('id', 'name',)  # 修正リンクでクリックできる項目
+
+
+admin.site.register(Movie, MovieAdmin)
+
+
+class ImpressionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment',)
+    list_display_links = ('id', 'comment',)
+    raw_id_fields = ('movie',)   # 外部キーをプルダウンにしない（データ件数が増加時のタイムアウトを予防）
+
+
+admin.site.register(Impression, ImpressionAdmin)
