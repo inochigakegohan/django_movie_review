bc65d3acf1dee0bad9fcd2e3f6e8abae7ece55bf
inochigakegohan <kishiura6202@gmail.com>
2024-06-20 09:19:11 +0900

    fix: コンパイルエラーの修正

--- movie_review/cms/models.py
+++ movie_review/cms/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Movie(models.Model):
     """映画"""
     name = models.CharField('映画名', max_length=255)
-    publisher = models.CharField('配給会社', max_length=255, blank=True)
-    page = models.IntegerField('上映時間', blank=True, default=0)
+    company = models.CharField('配給会社', max_length=255, blank=True)
+    time = models.IntegerField('上映時間', blank=True, default=0)
 
     def __str__(self):
         return self.name
@@ -13,7 +13,7 @@ class Movie(models.Model):
 
 class Impression(models.Model):
     """感想"""
-    book = models.ForeignKey(Movie, verbose_name='映画名', related_name='impressions', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, verbose_name='映画名', related_name='impressions', on_delete=models.CASCADE)
     comment = models.TextField('コメント', blank=True)
 
     def __str__(self):
