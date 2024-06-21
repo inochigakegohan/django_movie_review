af953fea8d70d15a6f4527e3431518c8e0b4af9c
inochigakegohan <kishiura6202@gmail.com>
2024-06-21 09:07:23 +0900

    feat: cmsをプロジェクトに登録

--- movie_review/cms/urls.py
+++ movie_review/cms/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+
 from movie_review.cms import views
 
 app_name = 'cms'
 urlpatterns = [
     # 映画
-    path('movie/', views.movie_list, name='movie_list'),   # 一覧
+    path('cms/', include('cms.urls')),
+    path('movie/', views.movie_list, name='movie_list'),  # 一覧
     path('movie/add/', views.movie_edit, name='movie_add'),  # 登録
     path('movie/mod/<int:movie_id>/', views.movie_edit, name='movie_mod'),  # 修正
-    path('movie/del/<int:movie_id>/', views.movie_del, name='movie_del'),   # 削除
+    path('movie/del/<int:movie_id>/', views.movie_del, name='movie_del'),  # 削除
 ]
