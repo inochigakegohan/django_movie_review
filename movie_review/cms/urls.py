09d48dc366df60d844f87eb6ec909246d5bf4fdf
inochigakegohan <kishiura6202@gmail.com>
2024-06-22 08:45:13 +0900

    fix: 更新するurlsが間違ってたので修正

--- movie_review/cms/urls.py
+++ movie_review/cms/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path, include
-
-from movie_review.cms import views
+from django.urls import path
+from cms import views
 
 app_name = 'cms'
 urlpatterns = [
     # 映画
-    path('cms/', include('cms.urls')),
     path('movie/', views.movie_list, name='movie_list'),  # 一覧
     path('movie/add/', views.movie_edit, name='movie_add'),  # 登録
     path('movie/mod/<int:movie_id>/', views.movie_edit, name='movie_mod'),  # 修正
