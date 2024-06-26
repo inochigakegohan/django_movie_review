3649e8b15f9f63573b3c6f88038058af4adea7af
inochigakegohan <kishiura6202@gmail.com>
2024-06-26 09:07:28 +0900

    feat: モデルの作成

--- movie/movie_review/cms/models.py
+++ movie/movie_review/cms/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """作品"""
+    name = models.CharField('作品名', max_length=255)
+    company = models.CharField('配給会社', max_length=255, blank=True)
+    time = models.IntegerField('上映時間', blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Impression(models.Model):
+    """感想"""
+    movie = models.ForeignKey(Movie, verbose_name='作品', related_name='impressions', on_delete=models.CASCADE)
+    comment = models.TextField('コメント', blank=True)
+
+    def __str__(self):
+        return self.comment
+
