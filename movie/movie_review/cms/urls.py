2b0ee5f1ac8163cc5ed1597724836956b5d150be
inochigakegohan <kishiura6202@gmail.com>
2024-07-01 09:56:01 +0900

    feat: URLと、ビューのファンクションの紐付けを行う

---
+++ movie/movie_review/cms/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from cms import views
+
+app_name = 'cms'
+urlpatterns = [
+    # 映画
+    path('movie/', views.movie_list, name='movie_list'),   # 一覧
+    path('movie/add/', views.movie_edit, name='movie_add'),  # 登録
+    path('movie/mod/<int:movie_id>/', views.movie_edit, name='movie_mod'),  # 修正
+    path('movie/del/<int:movie_id>/', views.movie_del, name='movie_del'),   # 削除
+]
