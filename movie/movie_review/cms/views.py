75158007fc7aa4a8c7e324365970713284b9cf3c
inochigakegohan <kishiura6202@gmail.com>
2024-07-01 09:53:51 +0900

    feat: 登録、修正、削除のひな形を作り、一覧の表示ビューを作成

--- movie/movie_review/cms/views.py
+++ movie/movie_review/cms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from cms.models import Movie
+
+def movie_list(request):
+    """映画の一覧"""
+    # return HttpResponse('映画の一覧')
+    movies = Movie.objects.all().order_by('id')
+    return render(request,
+                  'cms/movie_list.html',  # 使用するテンプレート
+                  {'movies': movies})  # テンプレートに渡すデータ
+
+
+def movie_edit(request, movie_id=None):
+    """映画の編集"""
+    return HttpResponse('映画の編集')
+
+
+def movie_del(request, movie_id):
+    """映画の削除"""
+    return HttpResponse('映画の削除')
