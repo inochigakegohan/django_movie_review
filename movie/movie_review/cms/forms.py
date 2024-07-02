66a1c7c6c4b1139332b7e4d5df9c1c7c787ef664
inochigakegohan <kishiura6202@gmail.com>
2024-07-02 09:49:56 +0900

    feat:  Impression モデルを追加、修正するための元となるフォームを作成

--- movie/movie_review/cms/forms.py
+++ movie/movie_review/cms/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
-from cms.models import Movie
-
+from cms.models import Movie, Impression
 
 class MovieForm(ModelForm):
     """映画のフォーム"""
+
     class Meta:
         model = Movie
-        fields = ('name', 'company', 'time', )
+        fields = ('name', 'company', 'time',)
+
+        class ImpressionForm(ModelForm):
+            """感想のフォーム"""
+
+            class Meta:
+                model = Impression
+                fields = ('comment',)
