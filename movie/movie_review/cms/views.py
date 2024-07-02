d7d4d678c99dbcd2244422ed7c11af0ca0cc5c3d
inochigakegohan <kishiura6202@gmail.com>
2024-07-02 09:02:53 +0900

    feat: 映画の追加、修正を行うビューの作成

--- movie/movie_review/cms/views.py
+++ movie/movie_review/cms/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from cms.models import Movie
+from cms.forms import MovieForm
+
 
 def movie_list(request):
     """映画の一覧"""
@@ -14,7 +16,22 @@ def movie_list(request):
 
 def movie_edit(request, movie_id=None):
     """映画の編集"""
-    return HttpResponse('映画の編集')
+    # return HttpResponse('映画の編集')
+    if movie_id:  # movie_id が指定されている (修正時)
+        movie = get_object_or_404(Movie, pk=movie_id)
+    else:  # movie_id が指定されていない (追加時)
+        movie = Movie()
+
+    if request.method == 'POST':
+        form = MovieForm(request.POST, instance=movie)  # POST された request データからフォームを作成
+        if form.is_valid():  # フォームのバリデーション
+            movie = form.save(commit=False)
+            movie.save()
+            return redirect('cms:movie_list')
+    else:  # GET の時
+        form = MovieForm(instance=movie)  # movie インスタンスからフォームを作成
+
+    return render(request, 'cms/movie_edit.html', dict(form=form, movie_id=movie_id))
 
 
 def movie_del(request, movie_id):
