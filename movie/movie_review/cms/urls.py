0cd28aa6304db00d0e3aad19c6aa36303c914e7d
inochigakegohan <kishiura6202@gmail.com>
2024-07-03 09:10:45 +0900

    feat: 感想のURLスキームを作成

--- movie/movie_review/cms/urls.py
+++ movie/movie_review/cms/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('movie/add/', views.movie_edit, name='movie_add'),  # 登録
     path('movie/mod/<int:movie_id>/', views.movie_edit, name='movie_mod'),  # 修正
     path('movie/del/<int:movie_id>/', views.movie_del, name='movie_del'),   # 削除
+    # 感想
+    path('impression/<int:movie_id>/', views.ImpressionList.as_view(), name='impression_list'),  # 一覧
+    path('impression/add/<int:movie_id>/', views.impression_edit, name='impression_add'),        # 登録
+    path('impression/mod/<int:movie_id>/<int:impression_id>/', views.impression_edit, name='impression_mod'),  # 修正
+    path('impression/del/<int:movie_id>/<int:impression_id>/', views.impression_del, name='impression_del'),   # 削除
 ]
