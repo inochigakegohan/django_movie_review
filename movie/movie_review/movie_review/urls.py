11a9bbca4260c80b8316ffc5982bda936a0b7ffe
inochigakegohan <kishiura6202@gmail.com>
2024-07-09 09:00:17 +0900

    feat: api/urlsをurlsに追加

--- movie/movie_review/movie_review/urls.py
+++ movie/movie_review/movie_review/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include   # ←, includeを追加
 
 urlpatterns = [
-    path('cms/', include('cms.urls')),   # ←ここを追加
+    path('cms/', include('cms.urls')),
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
